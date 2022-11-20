fabde2cb636220dd0fdab45230f1e9eb270d6728
Dimitar <dim2517@gmail.com>
2022-11-20 12:10:56 +0200

    Created table component with default value if table is empty

---
+++ frontend/src/components/Table.vue
@@ -0,0 +1,44 @@
+<template>
+    <table class="table table-striped table-responsive ">
+        <thead>
+            <tr>
+                <th class="p-2 text-center" scope="col">Date</th>
+                <th class="p-2 text-center" scope="col">Holiday Name</th>
+                <th class="p-2 text-center" scope="col">Holiday Type</th>
+            </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-200" id="table-body">
+            <tr v-if="dates.length > 0" v-for="(date, key) in dates" :key="key">
+                <td class="whitespace-nowrap p-2 text-center" >
+                    {{ date.date.day }}-{{ date.date.month }}-{{ date.date.year }}
+                </td>
+                <td class="whitespace-nowrap p-2 text-center">
+                    {{ date.name[0].text }}
+                </td>
+                <td class="whitespace-nowrap p-2 text-center">
+                    {{ date.holidayType }}
+                </td>
+            </tr>
+            <tr v-else>
+                <td class="p-2 text-center" colspan="3">Data pending...</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "Table",
+    props: {
+        dates: {type: Array}
+    }
+}
+</script>
+
+<style scoped>
+th {
+    background-color: deepskyblue;
+    color: white;
+}
+</style>
+
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,32 +5,11 @@
                 <label>Select public holiday year</label>
                 <input type="date"
                        class="form-control"
-                       @change="selectDate(date)"
-                       v-model="date"
+                       @change="selectDate(dateInput)"
+                       v-model="dateInput"
                 />
             </div>
-            <table class="table table-striped table-dark table-responsive ">
-                <thead>
-                    <tr>
-                        <th scope="col">Date</th>
-                        <th scope="col">Holiday Name</th>
-                        <th scope="col">Holiday Type</th>
-                    </tr>
-                </thead>
-                <tbody class="divide-y divide-gray-200" id="table-body">
-                    <tr v-for="date in dates" :key="date.date.day">
-                        <td class="whitespace-nowrap" >
-                            {{ date.date.day }}-{{ date.date.month }}-{{ date.date.year }}
-                        </td>
-                        <td class="whitespace-nowrap">
-                            {{ date.name[0].text }}
-                        </td>
-                        <td class="whitespace-nowrap">
-                            {{ date.holidayType }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <Table :dates="dates"></Table>
         </div>
     </div>
 </template>
@@ -39,20 +18,16 @@
 <script setup>
 import {ref, onMounted} from 'vue';
 import axios from 'axios';
+import Table from "@/components/Table.vue";
 
-// Set empty reactive property for date input
-let date = ref('');
+let dateInput = ref('');
+const holidays = ref({});
 
-// Set reactive property for dates collection
-const dates = ref({});
-
-// Create select date function
+// Compute the date input
 const selectDate = (date) => {
-    // Only get the year from input
-    let getSelectedYear = date.split("-");
-
-    // fetch data from laravel api and update dates object
-    ajaxRequest(getSelectedYear[0]);
+    // Only get the year from input string
+    let getYear = date.split("-");
+    ajaxRequest(getYear[0]);
 }
 
 // Reusable ajax function for getting api data
@@ -62,8 +37,8 @@ const ajaxRequest = (year = new Date().getFullYear()) => {
             params:{'year': year}
         })
         .then((response) => {
-            // Update the dates property with new data received from the api
-            dates.value = response.data
+            // Update the holidays property with new data from the api
+            holidays.value = response.data
         })
         .catch((error) => {
             // Log error
@@ -72,9 +47,10 @@ const ajaxRequest = (year = new Date().getFullYear()) => {
 }
 
 onMounted(() => {
-    // If dates object is empty set default value from api
-    if(Object.keys(dates.value).length === 0
-        && dates.value.constructor === Object){
+    // If holiday object is empty get current year holidays by default
+    if(Object.keys(holidays.value).length === 0 &&
+        holidays.value.constructor === Object)
+    {
         ajaxRequest();
     }
 })
